131328285a20c442f6781b50100a71b04d1301f0
Paradox <xzq351107836@163.com>
2018-09-02 12:37:56 +0800

    44444

--- app/src/main/java/top/paradoxie/www/helpdeveloper/MainActivity.java
+++ app/src/main/java/top/paradoxie/www/helpdeveloper/MainActivity.java
@@ -1,7 +1,11 @@
 package top.paradoxie.www.helpdeveloper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.alibaba.android.arouter.launcher.ARouter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView button = findViewById(R.id.test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ARouter.getInstance().build("/app/TestActivity").navigation();
+            }
+        });
     }
 }
